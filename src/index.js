2723eee78db5b7358fab6d1c4934068ea150069d
GourangaRoy1234 <mechgouranga1234@gmail.com>
2025-12-12 07:29:28 +0000

    mongoDb is now connected

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv"
 
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./.env"
 })
 
 const PORT=process.env.PORT || 7000
-app.listen(PORT,()=>{
-    console.log(`SERVER IS RUNNING ON ${PORT}`)
+
+connectDB() 
+// connect Mongodb is not a funtion is occur due to after mongo use mis ()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server is running on port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection error ",err)
 })
